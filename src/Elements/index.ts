0d726f6ddb3b717bdb381e744a457039287bd306
Stephen Muchendu <muchendumaina66@gmail.com>
2022-11-15 14:34:13 +0300

    Updates

---
+++ src/Elements/index.ts
@@ -0,0 +1,8 @@
+import img1 from './imgs/1.jpeg';
+import img2 from './imgs/2.jpeg';
+import img3 from './imgs/3.jpg';
+import img4 from './imgs/4.jpeg';
+import img5 from './imgs/5.jpeg';
+import logo from './imgs/logo.svg';
+
+export const imgs = { img1, img2, img3, img4, img5, logo };
