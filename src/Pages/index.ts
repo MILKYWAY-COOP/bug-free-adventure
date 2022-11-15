f7a0a04aafdcf2ac801b1b87b5b3dca9c28a9615
Stephen Muchendu <muchendumaina66@gmail.com>
2022-11-15 14:14:19 +0300

    Index file for page

---
+++ src/Pages/index.ts
@@ -0,0 +1,4 @@
+export { default as Bio } from './Bio/Bio';
+export { default as Portfolio } from './Portfolio/Portfolio';
+export { default as Wallpapers } from './Wallpapers/Wallpapers';
+export { default as Error } from './Error/Error';
