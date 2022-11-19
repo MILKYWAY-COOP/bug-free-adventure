f11093affffcecd41750f555530add6a293962fc
Stephen Muchendu <muchendumaina66@gmail.com>
2022-11-19 15:34:22 +0300

    updates

--- src/Pages/Portfolio/Portfolio.styled.ts
+++ src/Pages/Portfolio/Portfolio.styled.ts
@@ -1,3 +1,26 @@
 import styled from 'styled-components';
 
-export const ErrorContainer = styled.div``;
+export const PortfolioStyled = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  h1 {
+    color: var(--secondaryColor);
+    font-size: 1.5rem;
+    font-family: var(--secondaryFont);
+    margin: 1rem 0;
+  }
+
+  .picsGrid {
+    width: 100%;
+    height: fit-content;
+    display: grid;
+    grid-template-columns: repeat(3, 1fr);
+
+    img {
+      width: 100%;
+      height: 300px;
+    }
+  }
+`;
