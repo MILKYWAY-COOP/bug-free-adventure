cd0965341b7cf418f93be2d39e79504b39fbbedb
Stephen Muchendu <muchendumaina66@gmail.com>
2022-11-19 15:34:25 +0300

    updates

--- src/Pages/Portfolio/Portfolio.tsx
+++ src/Pages/Portfolio/Portfolio.tsx
@@ -1,3 +1,18 @@
+import { PortfolioStyled } from './Portfolio.styled';
+import { imgs } from '../../Elements';
+
 export default function Portfolio() {
-  return <div>Portfolio</div>;
+  const { img1, img2, img3, img4, img5 } = imgs;
+  const images = [img1, img2, img3, img4, img5];
+
+  return (
+    <PortfolioStyled>
+      <h1>Here I show off some of my favorite shots. Enjoy!</h1>
+      <div className='picsGrid'>
+        {images.map((img, i) => (
+          <img src={img} alt={`pic${i}`} key={i} />
+        ))}
+      </div>
+    </PortfolioStyled>
+  );
 }
