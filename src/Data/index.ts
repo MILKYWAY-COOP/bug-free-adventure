9e05944b7290b00238441c78b0cf56e69f5b5c6a
Stephen Muchendu <muchendumaina66@gmail.com>
2023-01-13 22:11:19 +0300

    added images array

---
+++ src/Data/index.ts
@@ -0,0 +1,29 @@
+import img1 from './images/6 (1).jpg';
+import img2 from './images/6 (2).jpg';
+import img3 from './images/6 (3).jpg';
+import img4 from './images/6 (4).jpg';
+import img5 from './images/6 (5).jpg';
+import img6 from './images/6 (6).jpg';
+import img7 from './images/6 (7).jpg';
+import img8 from './images/6 (8).jpg';
+import img9 from './images/6 (9).jpg';
+import img10 from './images/6 (10).jpg';
+import img11 from './images/6 (11).jpg';
+import img12 from './images/6 (12).jpg';
+import img13 from './images/6 (13).jpg';
+
+export const images = [
+  { url: img1, description: 'This is the image description. ***********' },
+  { url: img2, description: 'This is the image description. ***********' },
+  { url: img3, description: 'This is the image description. ***********' },
+  { url: img4, description: 'This is the image description. ***********' },
+  { url: img5, description: 'This is the image description. ***********' },
+  { url: img6, description: 'This is the image description. ***********' },
+  { url: img7, description: 'This is the image description. ***********' },
+  { url: img8, description: 'This is the image description. ***********' },
+  { url: img9, description: 'This is the image description. ***********' },
+  { url: img10, description: 'This is the image description. ***********' },
+  { url: img11, description: 'This is the image description. ***********' },
+  { url: img12, description: 'This is the image description. ***********' },
+  { url: img13, description: 'This is the image description. ***********' }
+];
