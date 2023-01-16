554d7192d5f9fd5eb3038c1734a0258a37a944c3
Stephen Muchendu <muchendumaina66@gmail.com>
2023-01-16 14:12:40 +0300

    added styles for wallpaper

--- src/Pages/Wallpapers/Wallpapers.styled.ts
+++ src/Pages/Wallpapers/Wallpapers.styled.ts
@@ -1,3 +1,25 @@
 import styled from 'styled-components';
 
-export const ErrorContainer = styled.div``;
+export const WallpapersStyled = styled.div`
+  width: 100%;
+  height: fit-content;
+  margin-bottom: 3rem;
+
+  .picsGrid {
+    width: 100%;
+    height: fit-content;
+    display: grid;
+    grid-template-columns: repeat(3, 1fr);
+    grid-template-rows: repeat(auto-fit, 1fr);
+
+    @media screen and (max-width: 768px) {
+      grid-template-columns: repeat(2, 1fr);
+      grid-template-rows: repeat(auto-fit, 1fr);
+    }
+
+    @media screen and (max-width: 480px) {
+      grid-template-columns: repeat(1, 1fr);
+      grid-template-rows: repeat(auto-fit, 1fr);
+    }
+  }
+`;
