e7bf1d196932ed24a2e11fd545ec613b677112c5
Stephen Muchendu <muchendumaina66@gmail.com>
2023-01-16 14:08:32 +0300

    added exports for Socials and Footer

--- src/Components/index.ts
+++ src/Components/index.ts
@@ -1,2 +1,4 @@
 export { default as Navbar } from './Navbar/Navbar';
 export { default as Image } from './Image/Image';
+export { default as Socials } from './Socials/Socials';
+export { default as Footer } from './Footer/Footer';
