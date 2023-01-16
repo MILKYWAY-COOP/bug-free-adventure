4382d434528a2ff8d92dcf146e74f402cc6ab5cd
Stephen Muchendu <muchendumaina66@gmail.com>
2023-01-16 14:13:21 +0300

    Modified wallpapers component

--- src/Pages/Wallpapers/Wallpapers.tsx
+++ src/Pages/Wallpapers/Wallpapers.tsx
@@ -1,3 +1,20 @@
+import { Image } from '../../Components';
+import { images } from '../../Data';
+import { WallpapersStyled } from './Wallpapers.styled';
+
 export default function Wallpapers() {
-  return <div>Wallpapers</div>;
+  return (
+    <WallpapersStyled>
+      <div className='picsGrid'>
+        {images.map((img, i) => (
+          <Image
+            src={img.url}
+            alt={`pic${i}`}
+            key={i}
+            description={img.description}
+          />
+        ))}
+      </div>
+    </WallpapersStyled>
+  );
 }
