73405c7a187ad6ec4c2e6dfbf85fe4c5843a46c5
Stephen Muchendu <muchendumaina66@gmail.com>
2023-01-17 16:04:26 +0300

    Minor updates

--- src/Pages/Bio/Bio.styled.ts
+++ src/Pages/Bio/Bio.styled.ts
@@ -1,3 +1,19 @@
 import styled from 'styled-components';
 
-export const BioContainer = styled.div``;
+export const BioContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 0 1rem;
+
+  p {
+    margin: 0;
+    line-height: 1.5;
+    font-size: 16px;
+    color: var(--secondaryColor);
+    font-family: var(--secondaryFont);
+    font-weight: 300;
+    text-align: center;
+    margin-bottom: 1rem;
+  }
+`;
