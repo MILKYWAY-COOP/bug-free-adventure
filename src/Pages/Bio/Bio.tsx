d00dd8937989f4c852ed836fbfebc5e7380f7a16
Stephen Muchendu <muchendumaina66@gmail.com>
2023-01-17 16:04:22 +0300

    Minor updates

--- src/Pages/Bio/Bio.tsx
+++ src/Pages/Bio/Bio.tsx
@@ -1,5 +1,36 @@
+import { BioContainer } from './Bio.styled';
+
 export default function Bio() {
-  return <div>
-    <h1>Some Bio</h1>
-  </div>;
+  return (
+    <BioContainer>
+      <p>
+        At <b>Imagenation KE</b>, we believe that the art of photography is not
+        just a job, but a passion that we strive to perfect every day. We are
+        dedicated to telling visual stories and creating memories that will last
+        a lifetime. We are constantly pushing the boundaries of the industry,
+        always seeking new and innovative ways to capture the perfect shot.
+      </p>
+
+      <p>
+        We understand that our profession is constantly evolving and we are
+        always open to new ideas, constructive criticism, and compliments. We
+        believe that communication is key and we are always willing to listen to
+        our clients' needs and desires. We pride ourselves on providing a
+        personalized service that is tailored to each individual client. We are
+        passionate about our work and we are committed to providing the highest
+        quality of photography services. Whether you are looking to capture
+        precious moments of your family, or you are looking to showcase your
+        business in the best possible light, we are here to help.
+      </p>
+
+      <p>
+        We offer a wide range of photography services, from portrait photography
+        to commercial photography, and everything in between. So why wait? If
+        you are looking for high-quality photography services, look no further
+        than <b>Imagenation KE</b>. Give us a call today on <b>0700 806 240</b>{' '}
+        or Whatsapp Only <b>0740769304</b> to book your appointment. Let us help
+        you create memories that will last a lifetime.
+      </p>
+    </BioContainer>
+  );
 }
